74266a4c1078c977d229d37ae1b4d58218872094
Maximilian Amthor <amthormaximilian@gmail.com>
2024-11-01 13:48:26 +0100

    use rsync when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 # Contains $SERVER
 source .env
 
-# TODO: use rsync instead of ssh to copy files
 docker compose -f docker-compose-build.yml build
-docker save nginx:prod | bzip2 | ssh $SERVER docker load
-docker save flask-app:prod | bzip2 | ssh $SERVER docker load
-scp docker-compose-prod.yml $SERVER:~/bipix/
-ssh $SERVER "cd bipix; docker stack deploy -c <(docker compose --env-file ~/ENV/.env -f docker-compose-prod.yml config | sed -e '1!b' -e '/^name:/d') bipix"
+docker save nginx:prod > nginx-prod.tar
+docker save flask-app:prod > flask-app-prod.tar
+rsync -z --info=progress2 docker-compose-prod.yml flask-app-prod.tar nginx-prod.tar $SERVER:~/bipix/
+rm nginx-prod.tar
+rm flask-app-prod.tar
+ssh $SERVER "cd bipix; docker load -i nginx-prod.tar; docker load -i flask-app-prod.tar; docker stack deploy -c <(docker compose --env-file ~/ENV/.env -f docker-compose-prod.yml config | sed -e '1!b' -e '/^name:/d') bipix"
